332819797fd18aea561f60bb38f6a74ed9902804
Chase <chase@chaseburr.com>
2021-02-11 21:29:27 -0700

    setupdb conection and server

---
+++ index.js
@@ -0,0 +1,22 @@
+// Dependencies
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+require("dotenv").config({});
+
+const server = new ApolloServer({
+     typeDefs,
+     resolvers,
+});
+
+mongoose
+     .connect(process.env.MONGODB_SERVER_CONNECT, { useNewUrlParser: true })
+     .then(() => {
+          console.log("db connected");
+          return server.listen({ port: 3000 });
+     })
+     .then((res) => {
+          console.log(`Server running at ${res.url}`);
+     });
