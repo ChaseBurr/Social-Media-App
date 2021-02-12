b2a75e023b2a740b322ec5c94633ec15f8a53dfb
Chase <chase@chaseburr.com>
2021-02-12 10:39:24 -0700

    client init

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Server
+import ApolloProvider from "./ApolloProvider";
+
+ReactDOM.render(ApolloProvider, document.getElementById("root"));
