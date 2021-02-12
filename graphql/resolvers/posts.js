0f2d9706a0073bb73d9ff8d3c50b7abc4ac95097
Chase <chase@chaseburr.com>
2021-02-12 09:49:46 -0700

    implemented like resolver

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require("./../../models/Post");
 const checkAuth = require("./../../util/check-auth");
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 module.exports = {
      Query: {
@@ -54,6 +54,28 @@ module.exports = {
                } catch (error) {
                     throw new Error(error);
                }
+          },
+          async likePost(_, { postId }, context) {
+               const { username } = checkAuth(context);
+
+               const post = await Post.findById(postId);
+               if (post) {
+                    // checks if the post is already liked by user
+                    if (post.likes.find((like) => like.username === username)) {
+                         // unlike comment
+                         post.likes = post.likes.filter(
+                              (like) => like.username !== username
+                         );
+                    } else {
+                         // like post
+                         post.likes.push({
+                              username,
+                              createdAt: new Date().toISOString(),
+                         });
+                    }
+                    await post.save();
+                    return post;
+               } else throw new UserInputError("Post not found");
           },
      },
 };
