dd35ee66a2608a55e58a1109d1fa5007920c117c
Chase <chase@chaseburr.com>
2021-02-12 22:33:20 -0700

    Created AuthRoute to redirect page when user is logged in

---
+++ client/src/util/AuthRoute.js
@@ -0,0 +1,21 @@
+// check if user is logged in and redirect them to home page
+
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+import { AuthContext } from "./../context/auth";
+
+const AuthRoute = ({ component: Component }, ...rest) => {
+     const { user } = useContext(AuthContext);
+
+     return (
+          <Route
+               {...rest}
+               render={(props) =>
+                    user ? <Redirect to="/" /> : <Component {...props} />
+               }
+          />
+     );
+};
+
+export default AuthRoute;
