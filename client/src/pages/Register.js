59ca0d7ae289a3d9892a454960bc785dd9c01e7c
Chase <chase@chaseburr.com>
2021-02-12 21:26:07 -0700

    added Register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,117 @@
-import React from "react";
+import gql from "graphql-tag";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
 
-export default function Register() {
+import { useForm } from "./../util/hooks";
+
+import { Button, Form } from "semantic-ui-react";
+
+export default function Register(props) {
+     const [errors, setErrors] = useState({});
+
+     const { onChange, onSubmit, values } = useForm(registerUser, {
+          username: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+     });
+
+     const [addUser, { loading }] = useMutation(REGISTER_USER, {
+          update(proxy, result) {
+               props.history.push("/");
+          },
+          onError(err) {
+               console.log(err);
+               setErrors(err.graphQLErrors[0].extensions.exception.errors);
+          },
+          variables: values,
+     });
+
+     function registerUser() {
+          addUser();
+     }
+
      return (
-          <div>
-               <h1>register page</h1>
+          <div className="form-container">
+               <Form
+                    onSubmit={onSubmit}
+                    noValidate
+                    className={loading ? "loading" : ""}
+               >
+                    <h1>Register</h1>
+                    <Form.Input
+                         label="Username"
+                         placeholder="Username.."
+                         name="username"
+                         type="text"
+                         value={values.username}
+                         errors={errors.username ? true : false}
+                         onChange={onChange}
+                    />
+                    <Form.Input
+                         label="Email"
+                         placeholder="Email.."
+                         name="email"
+                         type="email"
+                         value={values.email}
+                         errors={errors.email ? true : false}
+                         onChange={onChange}
+                    />
+                    <Form.Input
+                         label="Password"
+                         placeholder="Password.."
+                         name="password"
+                         type="password"
+                         value={values.password}
+                         errors={errors.password ? true : false}
+                         onChange={onChange}
+                    />
+                    <Form.Input
+                         label="Confirm Password"
+                         placeholder="Confirm Password.."
+                         name="confirmPassword"
+                         type="password"
+                         value={values.confirmPassword}
+                         errors={errors.confirmPassword ? true : false}
+                         onChange={onChange}
+                    />
+                    <Button type="submit" primary>
+                         Register
+                    </Button>
+               </Form>
+               {Object.keys(errors).length > 0 && (
+                    <div className="ui error message">
+                         <div className="list">
+                              {Object.values(errors).map((value) => (
+                                   <li key={value}>{value}</li>
+                              ))}
+                         </div>
+                    </div>
+               )}
           </div>
      );
 }
+
+const REGISTER_USER = gql`
+     mutation register(
+          $username: String!
+          $email: String!
+          $password: String!
+          $confirmPassword: String!
+     ) {
+          register(
+               registerInput: {
+                    username: $username
+                    email: $email
+                    password: $password
+                    confirmPassword: $confirmPassword
+               }
+          ) {
+               id
+               email
+               username
+               createdAt
+               token
+          }
+     }
+`;
