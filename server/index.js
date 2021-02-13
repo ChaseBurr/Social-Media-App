3a3ddd4525ee378aedac8781be353cb13f4f7f83
Chase <chase@chaseburr.com>
2021-02-12 21:23:11 -0700

    updated server config

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const mongoose = require("mongoose");
 
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
+
 require("dotenv").config({});
+
+const PORT = process.env.PORT || 5000;
+const MONGODB = process.env.MONGODB_SERVER_CONNECT;
 
 const server = new ApolloServer({
      typeDefs,
@@ -13,11 +17,14 @@ const server = new ApolloServer({
 });
 
 mongoose
-     .connect(process.env.MONGODB_SERVER_CONNECT, { useNewUrlParser: true })
+     .connect(MONGODB, { useUnifiedTopology: true, useNewUrlParser: true })
      .then(() => {
-          console.log("db connected");
-          return server.listen({ port: 5000 });
+          console.log("MongoDB connected");
+          return server.listen({ port: PORT });
      })
      .then((res) => {
           console.log(`Server running at ${res.url}`);
+     })
+     .catch((err) => {
+          console.log(err);
      });
