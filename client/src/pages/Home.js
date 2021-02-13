db356c4882486dead7e54284d14a88d86ab8a53f
Chase <chase@chaseburr.com>
2021-02-12 21:25:39 -0700

    Updated Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,60 @@
+import React, { useContext } from "react";
+import { useQuery } from "@apollo/client";
+import { Grid, Image } from "semantic-ui-react";
+import PostCard from "./../components/PostCard";
+
+const gql = require("graphql-tag");
+
+// TODO put all queries into file
+const FETCH_POSTS_QUERY = gql`
+     {
+          getPosts {
+               id
+               body
+               createdAt
+               username
+               likes {
+                    username
+               }
+               comments {
+                    id
+                    username
+                    createdAt
+                    body
+               }
+          }
+     }
+`;
+
 export default function Home() {
+     const { loading, data: { getPosts: posts } = {} } = useQuery(
+          FETCH_POSTS_QUERY
+     );
+
+     if (posts) {
+          console.log(posts);
+     }
+
      return (
-          <div>
-               <h1>Home page</h1>
-          </div>
+          <Grid columns={3} divided>
+               <Grid.Row className="page-title">
+                    <h1>Recent Posts</h1>
+               </Grid.Row>
+               <Grid.Row>
+                    {loading ? (
+                         <h1>Loading posts..</h1>
+                    ) : (
+                         posts &&
+                         posts.map((post) => (
+                              <Grid.Column
+                                   key={post.id}
+                                   style={{ marginBottom: "30px" }}
+                              >
+                                   <PostCard post={post} />
+                              </Grid.Column>
+                         ))
+                    )}
+               </Grid.Row>
+          </Grid>
      );
 }
