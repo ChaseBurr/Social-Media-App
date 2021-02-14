ca25ee03cb066138b47d944e5d5665cf21b8f761
Chase <chase@chaseburr.com>
2021-02-13 18:05:39 -0700

    added post tranision / moved query to different file

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,39 +1,18 @@
 import React, { useContext } from "react";
 import { useQuery } from "@apollo/client";
-import { Grid, Image } from "semantic-ui-react";
+import { Grid, Transition } from "semantic-ui-react";
+
+import PostForm from "./../components/PostForm";
 import PostCard from "./../components/PostCard";
 
-const gql = require("graphql-tag");
-
-// TODO put all queries into file
-const FETCH_POSTS_QUERY = gql`
-     {
-          getPosts {
-               id
-               body
-               createdAt
-               username
-               likes {
-                    username
-               }
-               comments {
-                    id
-                    username
-                    createdAt
-                    body
-               }
-          }
-     }
-`;
+import { AuthContext } from "./../context/auth";
+import { FETCH_POSTS_QUERY } from "./../util/graphql";
 
 export default function Home() {
+     const { user } = useContext(AuthContext);
      const { loading, data: { getPosts: posts } = {} } = useQuery(
           FETCH_POSTS_QUERY
      );
-
-     if (posts) {
-          console.log(posts);
-     }
 
      return (
           <Grid columns={3} divided>
@@ -41,18 +20,29 @@ export default function Home() {
                     <h1>Recent Posts</h1>
                </Grid.Row>
                <Grid.Row>
+                    {
+                         // let user add post if logged in
+                         user && (
+                              <Grid.Column>
+                                   <PostForm />
+                              </Grid.Column>
+                         )
+                    }
+
                     {loading ? (
                          <h1>Loading posts..</h1>
                     ) : (
-                         posts &&
-                         posts.map((post) => (
-                              <Grid.Column
-                                   key={post.id}
-                                   style={{ marginBottom: "30px" }}
-                              >
-                                   <PostCard post={post} />
-                              </Grid.Column>
-                         ))
+                         <Transition.Group>
+                              {posts &&
+                                   posts.map((post) => (
+                                        <Grid.Column
+                                             key={post.id}
+                                             style={{ marginBottom: "30px" }}
+                                        >
+                                             <PostCard post={post} />
+                                        </Grid.Column>
+                                   ))}
+                         </Transition.Group>
                     )}
                </Grid.Row>
           </Grid>
