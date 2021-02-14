cc01843d0998359293209d0757db2f3fedb07aff
Chase <chase@chaseburr.com>
2021-02-13 18:01:37 -0700

    added comments

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -13,6 +13,7 @@ const httpLink = createHttpLink({
      uri: "http://localhost:5000",
 });
 
+// set token in the header
 const authLink = setContext(() => {
      const token = localStorage.getItem("jwtToken");
      return {
