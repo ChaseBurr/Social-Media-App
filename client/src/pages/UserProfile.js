433b9ff5a760e5a426a2b6db94532ddaf308713b
Chase <chase@chaseburr.com>
2021-02-17 17:58:23 -0700

    added user profile page

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
-import { Card, Image } from "semantic-ui-react";
+import { Card, Image, Form, Button } from "semantic-ui-react";
 import moment from "moment";
 import { AuthContext } from "./../context/auth";
 import { useMutation, useQuery } from "@apollo/client";
+
+import { useForm } from "./../util/hooks";
 
 import { USER_INFORMATION_QUERY } from "./../util/graphql";
 
@@ -21,12 +23,18 @@ export default function UserProfile(props) {
           }
      );
 
+     const { values, onChange, onSubmit } = useForm(callback, {
+          description: "",
+     });
+
+     function callback() {}
+
      return (
           <>
                {userData ? (
                     <>
                          <div style={{ paddingTop: "1rem" }} />
-                         <Card fluid>
+                         <Card fluid className="user-profile">
                               <Card.Content
                                    style={{
                                         display: "flex",
@@ -35,7 +43,7 @@ export default function UserProfile(props) {
                                    <Image
                                         src="https://react.semantic-ui.com/images/avatar/large/molly.png"
                                         size="small"
-                                        float="right"
+                                        float="left"
                                         style={{ borderRadius: "50%" }}
                                    />
                                    <div className="user-info-page">
@@ -49,12 +57,51 @@ export default function UserProfile(props) {
                                              ).fromNow()}
                                         </p>
                                         <p className="description">
-                                             {userData.description
-                                                  ? userData.Description
-                                                  : "No description added"}
+                                             {!userData.description ? (
+                                                  user?.username ===
+                                                  username ? (
+                                                       <Form
+                                                            onSubmit={onSubmit}
+                                                       >
+                                                            <h3>
+                                                                 Add a
+                                                                 description
+                                                            </h3>
+                                                            <Form.Field>
+                                                                 <Form.Input
+                                                                      placeholder="Add a description"
+                                                                      name="body"
+                                                                      onChange={
+                                                                           onChange
+                                                                      }
+                                                                      value={
+                                                                           values.body
+                                                                      }
+                                                                      // error={
+                                                                      //      error
+                                                                      //           ? true
+                                                                      //           : false
+                                                                      // }
+                                                                 />
+                                                                 <Button
+                                                                      type="submit"
+                                                                      color="teal"
+                                                                 >
+                                                                      SUBMIT
+                                                                 </Button>
+                                                            </Form.Field>
+                                                       </Form>
+                                                  ) : (
+                                                       <p>
+                                                            {username} doesn't
+                                                            have a description.
+                                                       </p>
+                                                  )
+                                             ) : (
+                                                  "No description added"
+                                             )}
                                         </p>
                                    </div>
-                                   <Card.Description>{}</Card.Description>
                               </Card.Content>
                          </Card>
                     </>
